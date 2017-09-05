8cb8b257cbb6f75473c19ac6dc6819280d9ac000
Patompong Savaengsuk <sp.patompong@gmail.com>
2017-09-05 20:25:49 +0700

    Update dependencies.

--- resources/assets/src/app/app.component.ts
+++ resources/assets/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app super fun yeah!!!';
+  title = 'app super fun yeah!!! super cool';
 }
